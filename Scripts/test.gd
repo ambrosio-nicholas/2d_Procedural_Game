7483de3ec51f3da43fa4ca0a72edbf500f80f3bc
ambrosio-nicholas <ambrosio.nicholas7@gmail.com>
2025-12-21 19:24:01 -0600

    Adjusted Test Plate Formation
    I've adjusted the tectonic plate formation some more, I'm going to try and start integrating it into the actual terrain generation

--- Scripts/test.gd
+++ Scripts/test.gd
@@ -8,13 +8,19 @@ var points : Array[Vector2i] = [] # The point that is the "centerish" of a conti
 var pointsDir : Array[Vector2i] = [] # The direction that a given point is "drifitng in"
 var warped_points : Array[Vector2] = []
 
-var noise := FastNoiseLite.new()
+var noise = FastNoiseLite.new()
+var rangeNoise = FastNoiseLite.new()
+
+@export var rangeCutoff = 0.25
 
 func _ready() -> void:
 	# Setup noise parameters for smooth warping
 	noise.seed = randi()
 	noise.fractal_octaves = 4
-	noise.frequency =.008
+	noise.frequency = .008
+	
+	rangeNoise.seed = randi()
+	rangeNoise.frequency = .008
 	
 	generatePoints()
 
@@ -34,27 +40,47 @@ func reset() -> void:
 	points.clear()
 	pointsDir.clear()
 	warped_points.clear()
+	noise.seed = randi()
+	rangeNoise.seed = randi()
 	for y in range(gridSizeY):
 		for x in range(gridSizeX):
 			set_cell(Vector2i(x, y), 0, Vector2i(5, 5))
 
 func generatePoints() -> void:
 	reset()
+	var point = null
+	var edge = randi() % 4
+	if edge == 0:
+		point =Vector2i(randi() % (gridSizeX / 10), randi() % gridSizeY)
+		points.push_front(point)
+		pointsDir.push_front(Vector2i(-1,0))
+	elif edge == 1:
+		point =Vector2i(randi() % (gridSizeX / 10) + (gridSizeX * 0.9), randi() % gridSizeY)
+		points.push_front(point)
+		pointsDir.push_front(Vector2i(-1,0))
+	elif edge == 2:
+		point =Vector2i(randi() % gridSizeX, randi() % (gridSizeY / 10))
+		points.push_front(point)
+		pointsDir.push_front(Vector2i(-1,0))
+	elif edge == 3:
+		point =Vector2i(randi() % gridSizeX, randi() % (gridSizeY / 10) + (gridSizeY * 0.9))
+		points.push_front(point)
+		pointsDir.push_front(Vector2i(-1,0))
 	
-	for i in range(numOfPoints):
-		var point = Vector2i(randi() % gridSizeX, randi() % gridSizeY)
+	for i in range(numOfPoints - 1):
+		point = Vector2i(randi() % gridSizeX, randi() % gridSizeY)
 		points.push_back(point)
 		# Generate random direction vector 
 		var direction = randi() % 4
 		match  direction:
 			0:
-				pointsDir.push_front(Vector2i(1,0))
+				pointsDir.push_back(Vector2i(1,0))
 			1:
-				pointsDir.push_front(Vector2i(-1,0))
+				pointsDir.push_back(Vector2i(-1,0))
 			2:
-				pointsDir.push_front(Vector2i(0,1))
+				pointsDir.push_back(Vector2i(0,1))
 			3:
-				pointsDir.push_front(Vector2i(0,-1))
+				pointsDir.push_back(Vector2i(0,-1))
 		
 	warped_points = []
 	for p in points:
@@ -76,7 +102,7 @@ func findClosestPoint(x: int, y: int, warped_points: Array) -> void:
 	var secondClosestDistance = INF
 	
 	for i in range(warped_points.size()):
-		var d = target.distance_to(warped_points[i])
+		var d = target.distance_squared_to(warped_points[i])
 		if d < closestDistance:
 			secondClosestDistance = closestDistance
 			secondClosestIndex = closestIndex
@@ -86,12 +112,12 @@ func findClosestPoint(x: int, y: int, warped_points: Array) -> void:
 			secondClosestDistance = d
 			secondClosestIndex = i
 	
-	var EDGE_THRESHOLD = 1  # increase to thicken edges
+	var EDGE_THRESHOLD = 500  # increase to thicken edges
 	
 	if abs(secondClosestDistance - closestDistance) < EDGE_THRESHOLD:
 		# This cell is on an edge
-		if (pointsDir[closestIndex] - pointsDir[secondClosestIndex]) != Vector2i(0,0):
-			set_cell(Vector2i(x, y), 0, Vector2i(0, 3))  # Black tile for edge
+		if (rangeNoise.get_noise_2d(x,y) > rangeCutoff) && (pointsDir[closestIndex] != pointsDir[secondClosestIndex]):
+			set_cell(Vector2i(x, y), 0, Vector2i(0, 3))  # Red tile for mountainous edge
 		else:
 			set_cell(Vector2i(x, y), 0, Vector2i(0, 5))  # Black tile for edge
 	else:
@@ -104,4 +130,6 @@ func findClosestPoint(x: int, y: int, warped_points: Array) -> void:
 			Vector2i(1,0):
 				set_cell(Vector2i(x, y), 0, Vector2i(2, 4)) # Moving Right : Green
 			Vector2i(-1,0):
-				set_cell(Vector2i(x, y), 0, Vector2i(0, 0)) # Moving Left : Blue
+				set_cell(Vector2i(x, y), 0, Vector2i(4, 0)) # Moving Left : Gray
+	if closestIndex == 0:
+		set_cell(Vector2i(x, y), 0, Vector2i(0, 0)) # Moving Left : Gray
